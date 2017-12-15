59dcfac98718d7519c291f4418f6121a9dc55569
Situ William <situ_william@yahoo.com>
2017-12-15 13:08:32 -0500

    added text view

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,19 @@
 class BooksController < ApplicationController
-
+	def index
+		@books = Book.all
+		respond_to do |format|
+			format.html
+			format.text
+			format.json do
+				render json: @books.map do |book|
+					{
+						title: book.title,
+						author: book.author,
+						already_read: book.already_read
+					}
+				end
+			end
+		end
+	end
 
 end
